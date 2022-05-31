134e3a28613a646b1132b1f03180a3b2262233c2
Iryna Stein <iryna@sensibleweather.com>
2022-05-31 11:04:34 -0600

    added checkbox test

--- src/App.js
+++ src/App.js
@@ -4,15 +4,25 @@ import { useState } from 'react';
 
 function App() {
   const [background, setBackground] = useState('red');
+  const [disabled, setDisabled] = useState(false);
   const newButtonColor = background === 'red' ? 'blue' : 'red';
   return (
     <div>
       <button
         style={{ backgroundColor: background }}
         onClick={() => setBackground(newButtonColor)}
+        disabled={disabled}
       >
         Change to {newButtonColor}
       </button>
+      <br />
+      <input
+        type="checkbox"
+        id="enable-button-checkbox"
+        defaultChacked={disabled}
+        aria-checked={disabled}
+        onChange={(e) => setDisabled(e.target.checked)}
+      />
     </div>
   );
 }
